faa916e545e208071da89c85ca49bd622d15277c
zengsl <zengsl0212@foxmail.com>
2023-11-25 21:45:10 +0800

    docs(db): modify

--- content-checker-mapper/src/main/resources/db/schema-mysql.sql
+++ content-checker-mapper/src/main/resources/db/schema-mysql.sql
@@ -1,78 +1,183 @@
-drop table  if exists  paper_info ;
-create table paper_info
+create schema `content-checker`;
+
+
+create table `content-checker`.check_paragraph
 (
-    paper_id     bigint auto_increment comment '论文主键',
-    paper_no     VARCHAR(256)                       null comment '论文编号',
+    paragraph_id  bigint auto_increment comment '段落主键'
+        primary key,
+    paragraph_num int                                null comment '段落号',
+    paper_no      varchar(256)                       null comment '论文编号',
+    task_id       bigint                             null comment '检测任务主键',
+    check_id      bigint                             null comment '检测数据主键',
+    content       text                               null comment '文本内容',
+    similarity    double   default -1                null comment '相似度',
+    status        char     default '0'               null comment '状态: 0 待处理,  1 处理中， 2 已完成 3 失败',
+    create_time   datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time   datetime default CURRENT_TIMESTAMP null comment '更新时间',
+    hash          bigint                             null comment 'hash值',
+    hash1         varchar(20)                        null comment '第一段hash值',
+    hash2         varchar(20)                        null comment '第二段hash值',
+    hash3         varchar(20)                        null comment '第三段hash值',
+    hash4         varchar(20)                        null comment '第四段hash值'
+)
+    comment '检测文本段落';
+
+create table `content-checker`.check_paragraph_pair
+(
+    id             bigint auto_increment comment '主键'
+        primary key,
+    task_id        bigint                             null comment '验证任务主键',
+    check_para_id  bigint                             null comment '验证段落主键',
+    target_para_id bigint                             null comment '疑似段落主键',
+    similarity     double                             null comment '相似度',
+    status         char     default '0'               null comment '状态: 0 待处理,  1 处理中， 2 已完成 3 失败',
+    create_time    datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time    datetime default CURRENT_TIMESTAMP null comment '更新时间'
+)
+    comment '验证段落对';
+
+create table `content-checker`.check_request
+(
+    check_id     bigint auto_increment comment '验证请求主键'
+        primary key,
+    check_no     varchar(50)                        null comment '验证标号',
+    paper_no     varchar(256)                       null comment '论文编号',
+    req_source   char     default '1'               null comment '请求来源: 1 web端调用 2 系统API',
+    title        varchar(400)                       null comment '标题',
+    author       varchar(150)                       null comment '作者',
+    publish_year char(4)                            null,
+    task_num     int      default 0                 null comment '任务数: check_task',
+    status       char     default '0'               null comment '状态: 0 待处理,  1 处理中， 2 已完成',
+    similarity   double   default -1                not null comment '相似度',
+    create_time  datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time  datetime default CURRENT_TIMESTAMP null comment '更新时间'
+)
+    comment '验证请求';
+
+create table `content-checker`.check_sentence
+(
+    sentence_id    bigint auto_increment comment '句子主键'
+        primary key,
+    sentence_num   int                                null comment '句子号',
+    paragraph_id   bigint                             null comment '段落主键',
+    origin_content text                               null comment '原始文本内容',
+    content        text                               null comment '文本内容',
+    similarity     double   default -1                null comment '相似度',
+    status         char     default '0'               null comment '状态: 0 待处理,  1 处理中， 2 已完成 3 失败',
+    create_time    datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time    datetime default CURRENT_TIMESTAMP null comment '更新时间'
+)
+    comment '检测文本句子';
+
+create table `content-checker`.check_sentence_pair
+(
+    id                 bigint auto_increment comment '主键'
+        primary key,
+    task_id            bigint                             null comment '验证任务主键',
+    check_para_id      bigint                             null comment '验证段落主键',
+    target_para_id     bigint                             null comment '疑似段落主键',
+    check_sentence_id  bigint                             null comment '验证句子主键',
+    target_sentence_id bigint                             null comment '疑似句子主键',
+    similarity         double   default -1                null comment '相似度',
+    status             char     default '0'               null comment '状态: 0 待处理,  1 处理中， 2 已完成 3 失败',
+    create_time        datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time        datetime default CURRENT_TIMESTAMP null comment '更新时间'
+)
+    comment '验证句子对';
+
+create table `content-checker`.check_task
+(
+    task_id     bigint auto_increment comment '验证任务主键'
+        primary key,
+    check_id    bigint                             null comment '验证请求主键',
+    check_no    varchar(50)                        null comment '验证标号',
+    paper_no    varchar(256)                       null comment '论文编号',
+    check_type  char     default '1'               null comment '验证类型: 1 内容 2 title',
+    content     text                               not null comment '内容',
+    similarity  double   default (-(1))            null comment '相似度',
+    status      char     default '0'               null comment '状态: 0 待处理,  1 处理中， 2 已完成 3 失败 4 取消',
+    create_time datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time datetime default CURRENT_TIMESTAMP null comment '更新时间'
+)
+    comment '验证任务';
+
+create table `content-checker`.paper_ext
+(
+    paper_id    bigint                             not null comment '论文主键'
+        primary key,
+    data_type   char(2)                            not null comment '数据类型',
+    content     text                               null comment '内容',
+    create_time datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time datetime default CURRENT_TIMESTAMP null comment '更新时间'
+)
+    comment '论文扩展信息';
+
+create table `content-checker`.paper_info
+(
+    paper_id     bigint auto_increment comment '论文主键'
+        primary key,
+    paper_no     varchar(256)                       null comment '论文编号',
     data_type    char(2)                            not null comment '数据类型',
     title        varchar(400)                       null comment '标题',
     author       varchar(150)                       null comment '作者',
     content      text                               null comment '内容',
     publish_year char(4)                            null,
-    hash LONG                            null comment 'hash值',
-    create_time  DATETIME default CURRENT_TIMESTAMP null comment '创建时间',
-    update_time  DATETIME default CURRENT_TIMESTAMP null comment '更新时间',
-    constraint paper_info_pk
-        primary key (paper_id)
+    data_source  char(2)                            not null comment '数据来源',
+    hash         bigint                             null comment 'hash值',
+    hash1        varchar(20)                        null comment '第一段hash值',
+    hash2        varchar(20)                        null comment '第二段hash值',
+    hash3        varchar(20)                        null comment '第三段hash值',
+    hash4        varchar(20)                        null comment '第四段hash值',
+    create_time  datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time  datetime default CURRENT_TIMESTAMP null comment '更新时间'
 )
     comment '论文信息';
 
-drop table  if exists  paper_ext ;
-create table paper_ext
+create table `content-checker`.paper_paragraph
 (
-    paper_id     bigint comment '论文主键',
-    data_type    char(2)                            not null comment '数据类型',
-    content      text                               null comment '内容',
-    create_time  DATETIME default CURRENT_TIMESTAMP null comment '创建时间',
-    update_time  DATETIME default CURRENT_TIMESTAMP null comment '更新时间',
-    constraint paper_ext_pk
-        primary key (paper_id)
-)
-    comment '论文扩展信息';
-
-
-drop table  if exists  paper_paragraph ;
-create table paper_paragraph
-(
-    paragraph_id     bigint auto_increment comment '段落主键',
-    paragraph_num     int comment '段落号',
-    paper_id     bigint comment '论文主键',
-    content      text                               null comment '文本内容',
-    create_time  DATETIME default CURRENT_TIMESTAMP null comment '创建时间',
-    update_time  DATETIME default CURRENT_TIMESTAMP null comment '更新时间',
-    hash LONG                            null comment 'hash值',
-    constraint paper_paragraph_pk
-        primary key (paragraph_id)
+    paragraph_id  bigint auto_increment comment '段落主键'
+        primary key,
+    paragraph_num int                                null comment '段落号',
+    paper_id      bigint                             null comment '论文主键',
+    paper_no      varchar(256)                       null comment '论文编号',
+    content       text                               null comment '文本内容',
+    create_time   datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time   datetime default CURRENT_TIMESTAMP null comment '更新时间',
+    hash          bigint                             null comment 'hash值',
+    hash1         varchar(20)                        null comment '第一段hash值',
+    hash2         varchar(20)                        null comment '第二段hash值',
+    hash3         varchar(20)                        null comment '第三段hash值',
+    hash4         varchar(20)                        null comment '第四段hash值'
 )
     comment '论文段落';
 
-drop table  if exists  paper_sentence ;
-create table paper_sentence
+create table `content-checker`.paper_sentence
 (
-    sentence_id     bigint auto_increment comment '句子主键',
-    sentence_num     int comment '句子号',
-    paragraph_id     bigint comment '段落主键',
-    content      text                               null comment '文本内容',
-    create_time  DATETIME default CURRENT_TIMESTAMP null comment '创建时间',
-    update_time  DATETIME default CURRENT_TIMESTAMP null comment '更新时间',
-    hash LONG                            null comment 'hash值',
-    constraint paper_sentence_pk
-        primary key (sentence_id)
+    sentence_id    bigint auto_increment comment '句子主键'
+        primary key,
+    sentence_num   int                                null comment '句子号',
+    paragraph_id   bigint                             null comment '段落主键',
+    origin_content text                               null comment '原始文本内容',
+    content        text                               null comment '文本内容',
+    hash           bigint                             null comment 'hash值',
+    hash1          varchar(20)                        null comment '第一段hash值',
+    hash2          varchar(20)                        null comment '第二段hash值',
+    hash3          varchar(20)                        null comment '第三段hash值',
+    hash4          varchar(20)                        null comment '第四段hash值',
+    create_time    datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time    datetime default CURRENT_TIMESTAMP null comment '更新时间'
 )
     comment '论文句子';
 
-
-
-drop table  if exists  paper_token ;
-create table paper_token
+create table `content-checker`.paper_token
 (
-    token_id     bigint auto_increment comment '分词主键',
-    token_num     int comment '分词序号',
-    sentence_id     bigint comment '句子主键',
+    token_id     bigint auto_increment comment '分词主键'
+        primary key,
+    token_num    int                                null comment '分词序号',
+    sentence_id  bigint                             null comment '句子主键',
+    paragraph_id bigint                             null comment '段落主键',
     content      text                               null comment '文本内容',
-    create_time  DATETIME default CURRENT_TIMESTAMP null comment '创建时间',
-    update_time  DATETIME default CURRENT_TIMESTAMP null comment '更新时间',
-    hash LONG                            null comment 'hash值',
-    constraint paper_token_pk
-        primary key (token_id)
+    create_time  datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time  datetime default CURRENT_TIMESTAMP null comment '更新时间'
 )
     comment '论文分词';
