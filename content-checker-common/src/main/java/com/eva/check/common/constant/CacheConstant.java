fc68069585ee0cd9a50f363a5f0f6476ba32ffd3
zengsl <zengsl0212@foxmail.com>
2024-04-30 15:33:08 +0800

    chore: 前缀修改

--- content-checker-common/src/main/java/com/eva/check/common/constant/CacheConstant.java
+++ content-checker-common/src/main/java/com/eva/check/common/constant/CacheConstant.java
@@ -9,11 +9,12 @@ package com.eva.check.common.constant;
  * @date 2024/4/18 10:34
  */
 public interface CacheConstant {
-    String CACHE_PREFIX = "Content_Check";
+    String CACHE_PREFIX = "CC";
 
     String PARAGRAPH_SENTENCE_CACHE_KEY = CACHE_PREFIX + ":para_sentence";
     String PARAGRAPH_TOKEN_CACHE_KEY = CACHE_PREFIX + ":para_token";
     String SENTENCE_TOKEN_CACHE_KEY = CACHE_PREFIX + ":sentence_paper_token";
+    String SENTENCE_CACHE_KEY = CACHE_PREFIX + ":sentence";
     String SENTENCE_PAPER_TOKEN_CACHE_KEY = CACHE_PREFIX + ":sentence_token";
     String SENTENCE_TOKEN_WORD_FREQ_CACHE_KEY = CACHE_PREFIX + ":sentence_token_word_freq";
     String REPORT_CONTENT_CACHE_KEY = CACHE_PREFIX + ":report_content";
